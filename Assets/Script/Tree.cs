6284cecf77e61eca7a3f9201b42408e00311d674
minji <5750molang@gmail.com>
2025-09-09 21:11:39 +0900

    인벤토리 시스템 제작중

---
+++ Assets/Script/Tree.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
