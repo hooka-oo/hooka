29468a4603f7e14f494e1fea3d47b8d21625983c
minji <5750molang@gmail.com>
2025-09-17 13:32:57 +0900

    수집 구현

--- Assets/1.Script/Player.cs
+++ Assets/1.Script/Player.cs
@@ -3,30 +3,38 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10f;
+    public Joystick joystick;
+
+    private void Awake()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
     void Start()
     {
         
     }
 
+    public Tree target;
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(1f, 0, 0) * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 1f, 0) * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(-1f, 0, 0) * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -1f, 0) * speed * Time.deltaTime;
-        }
+        transform.position += (Vector3)joystick.Direction * speed * Time.deltaTime;
 
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Player Update() F");
+            target.TakeDamage();
+        }
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collection")
+        {
+            Debug.Log("Player OnTriggerEnter2D()");
+            target = collision.GetComponent<Tree>();
+
+        }
+    }
 }
--- Assets/1.Script/Tree.cs
+++ Assets/1.Script/Tree.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class Tree : Resource
 {
-    
+    int hp = 3;
+    public Transform woodPrefab;
+
+    public void TakeDamage()
+    {
+        if (hp <= 0)
+            return;
+        if (gameObject.activeSelf == false)
+            return;
+        hp--;
+        if (hp <= 0)
+        {
+
+            gameObject.SetActive(false);
+            Transform wood = Instantiate(woodPrefab);
+            wood.transform.position = (Vector2)transform.position;
+            Debug.Log("Tree TakeDamage");
+        }
+    }
 }
